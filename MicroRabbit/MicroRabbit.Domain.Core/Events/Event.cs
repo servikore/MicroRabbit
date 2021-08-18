a3dc47fbafc166b01b5c587341e54bad9b2c9a53
Geuri Paulino <geruripaulino@gmail.com>
2021-08-18 18:16:48 -0400

    Created interfaces for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime TimeStamp { get; protected set; }
+        protected Event()
+        {
+            TimeStamp = DateTime.UtcNow;
+        }
+    }
+}
